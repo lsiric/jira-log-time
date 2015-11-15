633286775b1197066b5ec5e7e45254b1d1ff8ae6
lsiric <siric.luka@gmail.com>
2015-11-15 01:49:27 +0000

    cleanup

--- options.js
+++ options.js
@@ -1,42 +1,49 @@
-// Saves options to chrome.storage.sync.
-function save_options() {
-  var description = document.getElementById('description').value;
-  var jiraUrl = document.getElementById('jiraUrl').value;
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  chrome.storage.sync.set({
-    description: description,
-    jiraUrl: jiraUrl,
-    username: username,
-    password: password
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
+(function () {
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    description: '',
-    jiraUrl: '',
-    username: '',
-    password: ''
-  }, function(items) {
+    document.addEventListener('DOMContentLoaded', restoreOptions);
+    document.getElementById('save').addEventListener('click', saveOptions);
 
-    document.getElementById('description').value = items.description;
-    document.getElementById('jiraUrl').value = items.jiraUrl;
-    document.getElementById('username').value = items.username;
-    document.getElementById('password').value = items.password;
+    function saveOptions() {
 
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var description = document.getElementById('description').value;
+        var baseUrl = document.getElementById('baseUrl').value;
+        var apiExtension = document.getElementById('apiExtension').value;
+
+        chrome.storage.sync.set({
+            username: username,
+            password: password,
+            description: description,
+            baseUrl: baseUrl,
+            apiExtension: apiExtension
+        }, function() {
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function() {
+              status.textContent = '';
+            }, 750);
+        });
+    }
+
+
+    function restoreOptions() {
+
+        chrome.storage.sync.get({
+            username: '',
+            password: '',
+            description: '',
+            baseUrl: '',
+            apiExtension: '/rest/api/2'
+        }, function(items) {
+
+            document.getElementById('username').value = items.username;
+            document.getElementById('password').value = items.password;
+            document.getElementById('description').value = items.description;
+            document.getElementById('baseUrl').value = items.baseUrl;
+            document.getElementById('apiExtension').value = items.apiExtension;
+
+        });
+    }
+    
+})(); 
--- popup.js
+++ popup.js
@@ -1,216 +1,217 @@
-document.addEventListener('DOMContentLoaded', onDOMLoaded, false);
+(function () {
 
-
-
-function getCredentials () {
-    chrome.storage.sync.get({
-            description: '',
-            jiraUrl: '',
-            username: '',
-            password: ''
-        }, 
-        function(items) {
-            document.getElementById('description').value = items.description;
-            document.getElementById('jiraUrl').value = items.jiraUrl;
-            document.getElementById('username').value = items.username;
-            document.getElementById('password').value = items.password;
-    });
+    document.addEventListener('DOMContentLoaded', function () {
         
-}
+        chrome.storage.sync.get({
+                username: '',
+                password: '',
+                description: '',
+                baseUrl: '',
+                apiExtension: ''
+            }, 
+            function(items) {
 
+                var error = '';
 
-function onDOMLoaded () {
+                if(!items.username){
+                    error += 'Missing username';
+                }
+                if(!items.password){
+                    error += '; Missing password';
+                }
+                if(!items.baseUrl){
+                    error += '; Missing base URL';
+                }
+                if(!items.apiExtension){
+                    error += '; Missing API extension';
+                }
 
+                if(error){
+                    errorMessage(error);
+                }else{
+                    initPlugin(
+                        items.username,
+                        items.password,
+                        items.description,
+                        items.baseUrl,
+                        items.apiExtension
+                    );
+                }
 
-  // var checkPageButton = document.getElementById('checkPage');
-  // checkPageButton.addEventListener('click', function() {
-
-  //   chrome.tabs.getSelected(null, function(tab) {
-  //     d = document;
-
-  //     var f = d.createElement('form');
-  //     f.action = 'http://gtmetrix.com/analyze.html?bm';
-  //     f.method = 'post';
-  //     var i = d.createElement('input');
-  //     i.type = 'hidden';
-  //     i.name = 'url';
-  //     i.value = tab.url;
-  //     f.appendChild(i);
-  //     d.body.appendChild(f);
-  //     f.submit();
-  //   });
-  // }, false);
-
-
-
-    var username = 'cs.tropic@gmail.com';
-    var password = 'Creative1';
-    var description = 'Super Project';
-    var baseUrl = 'http://192.168.99.100:32768';
-    var apiExtension = '/rest/api/2';
-
-    var JIRA = JiraAPI(baseUrl, apiExtension, username, password);
-
-    JIRA.getAssignedIssues()
-    .success(function (issuesResponse) {
-
-        var promises = [];
-
-        issuesResponse.issues.forEach(function (issue) {
-            promises.push(getWorklog(issue.key));
         });
 
-        function getWorklog (id) {
-            return JIRA.getIssueWorklog(id)
-            .success(function (worklogResponse) {
+    }, false);
 
-                issuesResponse.issues.forEach(function (issue) {
-                    if(issue.key === id){
-                        issue.totalTime = sumWorklogs(worklogResponse.worklogs);
-                    } 
-                });
-            })
-            .error(function (error) {
-                console.log(error);
+
+    function initPlugin (username, password, description, baseUrl, apiExtension) {
+
+        var JIRA = JiraAPI(baseUrl, apiExtension, username, password);
+
+        JIRA.getAssignedIssues()
+        .success(function (issuesResponse) {
+
+            var promises = [];
+
+            issuesResponse.issues.forEach(function (issue) {
+                promises.push(getWorklog(issue.key));
             });
+
+            function getWorklog (id) {
+                return JIRA.getIssueWorklog(id)
+                .success(function (worklogResponse) {
+
+                    issuesResponse.issues.forEach(function (issue) {
+                        if(issue.key === id){
+                            issue.totalTime = sumWorklogs(worklogResponse.worklogs);
+                        } 
+                    });
+                })
+                .error(genericResponseError);
+            }
+
+            $.when.apply($, promises)
+            .done(function () {
+
+                drawIssuesTable(description, issuesResponse.issues);
+
+            });
+
+        })
+        .error(genericResponseError);
+
+
+        function drawIssuesTable (projectName, issues) {
+
+            var table = document.createElement('table');
+
+            addTableHeader(table, projectName);
+
+            for (var i = 0; i < issues.length; i++) {
+
+                var issue = issues[i];
+
+                addTableRow(
+                    table, 
+                    issue.key,
+                    issue.fields.summary,
+                    issue.totalTime
+                );
+
+            };
+
+            document.getElementById('issues').appendChild(table);
+
         }
 
-        $.when.apply($, promises)
-        .done(function () {
+        function addTableHeader (table, projectName) {
+            var header = document.createElement('th');
+            header.setAttribute('colspan', 3);
+            var td = document.createElement('td');
+            td.innerText = projectName;
+            header.appendChild(td);
+            table.appendChild(header);
+            return table;
+        }
 
-            drawIssuesTable(description, issuesResponse.issues);
+        function addTableRow (table, id, summary, time) {
+            var row = document.createElement('tr');
+            var td1 = document.createElement('td');
+            var td2 = document.createElement('td');
+            var td3 = document.createElement('td');
 
-        });
+            td1.innerText = id;
+            td1.className = 'issue-id';
 
-    })
-    .error(function (error) {
-        console.log(error);
-    });
+            td2.innerText = summary;
+            td2.className = 'issue-summary';
 
+            td3.innerText = time;
+            td3.className = 'issue-total-time-spent';
+            td3.setAttribute('data-issue-id', id);
 
-    function drawIssuesTable (projectName, issues) {
+            row.appendChild(td1);
+            row.appendChild(td2);
+            row.appendChild(td3);
 
-        var table = document.createElement('table');
+            addTimeInput(row, id);
+            addActionButton(row, id);
 
-        addTableHeader(table, projectName);
+            table.appendChild(row);
+            return table;
+        }
 
-        for (var i = 0; i < issues.length; i++) {
+        function addTimeInput (parent, issueId) {
+            var td = document.createElement('td');
+            
+            var input = document.createElement('input');
+            input.className = 'issue-time-input';
+            input.setAttribute('data-issue-id', issueId);
 
-            var issue = issues[i];
+            td.appendChild(input);
+            parent.appendChild(td);
+        }
 
-            addTableRow(
-                table, 
-                issue.key,
-                issue.fields.summary,
-                issue.totalTime
-            );
+        function addActionButton (parent, issueId) {
+            var td = document.createElement('td');
+            td.className = 'issue-actions';
+            
+            var button = document.createElement('button');
+            button.setAttribute('data-issue-id', issueId);
+            button.className = 'log-time-btn';
+            button.innerText = 'Log Time';
 
-        };
+            button.addEventListener('click', logTimeClick);
 
-        document.getElementById('issues').appendChild(table);
+            td.appendChild(button);
+            parent.appendChild(td);
+        }
 
-    }
+        function logTimeClick (evt) {
+            var issueId = evt.target.getAttribute('data-issue-id')
+            var timeInput = document.querySelector('input[data-issue-id=' + issueId + ']');
 
-    function addTableHeader (table, projectName) {
-        var header = document.createElement('th');
-        header.setAttribute('colspan', 3);
-        var td = document.createElement('td');
-        td.innerText = projectName;
-        header.appendChild(td);
-        table.appendChild(header);
-        return table;
-    }
+            JIRA.updateWorklog(issueId, timeInput.value)
+            .success(function (data) {
+                refreshWorklog(issueId);
+            })
+            .error(genericResponseError);
+        }
 
-    function addTableRow (table, id, summary, time) {
-        var row = document.createElement('tr');
-        var td1 = document.createElement('td');
-        var td2 = document.createElement('td');
-        var td3 = document.createElement('td');
+        function refreshWorklog (issueId) {
+            JIRA.getIssueWorklog(issueId)
+            .success(function (data) {
+                var totalTimeSpent = document.querySelector('td[data-issue-id=' + issueId + ']');
+                totalTimeSpent.innerText = sumWorklogs(data.worklogs);
+            })
+        }
 
-        td1.innerText = id;
-        td1.className = 'issue-id';
+        function sumWorklogs (worklogs) {
 
-        td2.innerText = summary;
-        td2.className = 'issue-summary';
+            var totalSeconds = worklogs.reduce(function(a, b){
+                return {timeSpentSeconds: a.timeSpentSeconds + b.timeSpentSeconds}
+            }, {timeSpentSeconds:0}).timeSpentSeconds;
 
-        td3.innerText = time;
-        td3.className = 'issue-total-time-spent';
-        td3.setAttribute('data-issue-id', id);
+            var totalWeeks = Math.floor(totalSeconds / 604800);
+            totalSeconds = totalSeconds % 604800;
+            var totalDays = Math.floor(totalSeconds / 86400);
+            totalSeconds = totalSeconds % 86400;
+            var totalHours = Math.floor(totalSeconds / 3600);
+            totalSeconds = totalSeconds % 3600;
+            var totalMinutes = Math.floor(totalSeconds / 60);
 
-        row.appendChild(td1);
-        row.appendChild(td2);
-        row.appendChild(td3);
+            return (totalWeeks ? totalWeeks + 'w' : '') + ' ' + (totalDays ? totalDays + 'd' : '') + ' ' + (totalHours ? totalHours + 'h' : '') + ' ' + (totalMinutes ? totalMinutes + 'min' : '');
 
-        addTimeInput(row, id);
-        addActionButton(row, id);
+        }
 
-        table.appendChild(row);
-        return table;
-    }
-
-    function addTimeInput (parent, issueId) {
-        var td = document.createElement('td');
-        
-        var input = document.createElement('input');
-        input.className = 'issue-time-input';
-        input.setAttribute('data-issue-id', issueId);
-
-        td.appendChild(input);
-        parent.appendChild(td);
-    }
-
-    function addActionButton (parent, issueId) {
-        var td = document.createElement('td');
-        td.className = 'issue-actions';
-        
-        var button = document.createElement('button');
-        button.setAttribute('data-issue-id', issueId);
-        button.className = 'log-time-btn';
-        button.innerText = 'Log Time';
-
-        button.addEventListener('click', logTimeClick);
-
-        td.appendChild(button);
-        parent.appendChild(td);
-    }
-
-    function logTimeClick (evt) {
-        var issueId = evt.target.getAttribute('data-issue-id')
-        var timeInput = document.querySelector('input[data-issue-id=' + issueId + ']');
-
-        JIRA.updateWorklog(issueId, timeInput.value)
-        .success(function (data) {
-            refreshWorklog(issueId);
-        })
-        .error(function (error) {
-            console.log(error);
-        });
-    }
-
-    function refreshWorklog (issueId) {
-        JIRA.getIssueWorklog(issueId)
-        .success(function (data) {
-            var totalTimeSpent = document.querySelector('td[data-issue-id=' + issueId + ']');
-            totalTimeSpent.innerText = sumWorklogs(data.worklogs);
-        })
-    }
-
-    function sumWorklogs (worklogs) {
-
-        var totalSeconds = worklogs.reduce(function(a, b){
-            return {timeSpentSeconds: a.timeSpentSeconds + b.timeSpentSeconds}
-        }, {timeSpentSeconds:0}).timeSpentSeconds;
-
-        var totalWeeks = Math.floor(totalSeconds / 604800);
-        totalSeconds = totalSeconds % 604800;
-        var totalDays = Math.floor(totalSeconds / 86400);
-        totalSeconds = totalSeconds % 86400;
-        var totalHours = Math.floor(totalSeconds / 3600);
-        totalSeconds = totalSeconds % 3600;
-        var totalMinutes = Math.floor(totalSeconds / 60);
-
-        return (totalWeeks ? totalWeeks + 'w' : '') + ' ' + (totalDays ? totalDays + 'd' : '') + ' ' + (totalHours ? totalHours + 'h' : '') + ' ' + (totalMinutes ? totalMinutes + 'min' : '');
+        function genericResponseError (error) {
+            var resp = JSON.parse(error.responseText);
+            errorMessage(resp.errorMessages[0]);
+        }
 
     }
 
-}
+    function errorMessage (message) {
+        document.getElementById('error').innerText = message;
+    }
+    
+})(); 
